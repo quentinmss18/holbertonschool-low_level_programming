96692681505099b0d1df18c9f50ebac7b9d06edf
quentinmss18 <quentinmoussous@gmail.com>
2025-10-28 10:48:12 +0000

    e

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,1 +1,19 @@
-int _isupper(int c);
+#include "main.h"
+
+/**
+ * _isdigit - function that checks for a digit 
+ * @c: it is the value for determines
+ * Return: 1 and 0
+ */
+
+int_isdigit(int c)
+{
+	if (c>= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return(0);
+	}
+}
