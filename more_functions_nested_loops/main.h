a6157c8f0e118c4250593a3536edaad1bc91285a
quentinmss18 <quentinmoussous@gmail.com>
2025-10-28 13:04:09 +0000

    task3

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -4,6 +4,9 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b); /* Prototype uniquement */
+/* Prototype de _putchar (souvent déjà fourni) */
+int _putchar(char c);
+/* Prototype de print_numbers */
 void print_numbers(void);
 #endif /* MAIN_H */
 
