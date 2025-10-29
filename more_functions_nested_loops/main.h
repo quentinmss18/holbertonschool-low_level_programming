96486d3dee47f8539691b35fd1facc61b6c9fb4c
quentinmss18 <quentinmoussous@gmail.com>
2025-10-29 12:17:36 +0000

    main.h

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -10,5 +10,12 @@ int _putchar(char c);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+int _putchar(char c);
+int _strlen(char *s);
+
 #endif /* MAIN_H */
 
