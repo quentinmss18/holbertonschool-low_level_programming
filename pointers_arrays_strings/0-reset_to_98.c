7babfea3d31831cbeff7a95b54d8a1c74c539254
quentinmss18 <quentinmoussous@gmail.com>
2025-10-30 11:01:52 +0000

    task0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - met la valeur pointée par n à 98
+ * @n: pointeur vers un entier
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+Ce projet fait partie du programme Holberton School.
+Il contient des exercices sur les pointeurs, les tableaux et les chaînes de caractères en langage 
