d96e7a20adfa094bebf58e46d753973994d5581d
quentinmss18 <quentinmoussous@gmail.com>
2025-10-30 12:27:38 +0000

    main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -3,6 +3,16 @@
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _putchar(char c);
 int _strlen(char *s);
 void _puts(char *str);
+int _putchar(char c);
+void print_rev(char *s);
+void rev_string(char *s);
+int _putchar(char c);
+void puts2(char *str);
+int _putchar(char c);
+void puts_half(char *str);
+void print_array(int *a, int n);
+int _atoi(char *s);
 #endif /* MAIN_H */
