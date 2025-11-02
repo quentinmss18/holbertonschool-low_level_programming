b180f8a0894ad46042dbd2750b536d9cb1a9a52f
quentinmss18 <quentinmoussous@gmail.com>
2025-11-02 00:27:50 +0000

    task0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -9,21 +9,21 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    int i, j;
+	int i, j;
 
-    i = 0;
-    while (dest[i] != '\0')
-        i++;
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
 
-    j = 0;
-    while (src[j] != '\0')
-    {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
-    dest[i] = '\0';
-    return (dest);
+	dest[i] = '\0';
+	return (dest);
 }
 
