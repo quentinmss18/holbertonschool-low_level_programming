93cf966fc093dfff6602ebcce13998e5f1c9ed30
quentinmss18 <quentinmoussous@gmail.com>
2025-11-07 15:03:21 +0000

    task3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, result;
+
+	if (argc != 3)
+	{
+	printf("Error\n");
+	return (1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	result = a * b;
+
+	printf("%d\n", result);
+
+	return (0);
+}
