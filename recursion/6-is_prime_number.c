bc865f34dae07c188a42b68c5c0397c67334ab25
quentinmss18 <quentinmoussous@gmail.com>
2025-11-07 14:36:53 +0000

    task6

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * is_prime_helper - checks recursively if n is divisible by i
+ * @n: number to check
+ * @i: current divisor to test
+ *
+ * Return: 1 if prime, 0 if not
+ */
+int is_prime_helper(int n, int i)
+{
+	if (n <= 1)
+	return (0);
+	if (i * i > n)
+	return (1);
+	if (n % i == 0)
+	return (0);
+	return (is_prime_helper(n, i + 1));
+}
+
+/**
+ * is_prime_number - returns 1 if n is prime, 0 otherwise
+ * @n: number to check
+ *
+ * Return: 1 if prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	return (is_prime_helper(n, 2));
+}
